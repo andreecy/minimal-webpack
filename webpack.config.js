5eeefc5186b423652976dd429938e8df7cd983ab
andreecy <andreechristianyulianto@gmail.com>
2019-04-07 00:13:11 +0700

    initial push

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Minimal Webpack App'
+    })
+  ],
+  output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+      rules: [
+        {
+            test: /\.css$/,
+            use: [
+            'style-loader',
+            'css-loader'
+            ]
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader'
+          ]
+        }
+      ]
+  }
+};
